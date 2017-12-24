0ab34ca71dae20c19c24c97092b3ec0741a2ffad
ChenBlue <bob840806@gmail.com>
2017-12-24 11:49:46 +0800

    data with different branch

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ sample_num = 20000;
 gI = normrnd(0,sqrt(var(i)),1,sample_num);
 
 enr_dB = 1;
-enr = 10^(enr_dB/10)
+enr = 10^(enr_dB/10);
 
 % generate QPSK data
 data = rand(2,sample_num); 
@@ -11,16 +11,18 @@ data = 2*(data > 0.5)-1;
 Edata = sqrt(2); % symbol energy
 En = Edata/enr; % noise energy
 
-% generate noise
-ni = normrnd(0,sqrt(En/2),2,sample_num);
-nq = normrnd(0,sqrt(En/2),2,sample_num);
+L = 2;
+ni = normrnd(0,sqrt(En/2),2,sample_num,L);
+nq = normrnd(0,sqrt(En/2),2,sample_num,L);
 n = ni + 1i*nq;
 
+%%
 % generate fading gain
-gi = normrnd(0,1,1,sample_num);
-gq = normrnd(0,1,1,sample_num);
+gi = normrnd(0,1,1,sample_num,L);
+gq = normrnd(0,1,1,sample_num,L);
 g = gi + 1i*gq;
-g = repmat(g,2,1);
+g = repmat(g,2,1,1);
+tx_data = repmat(data,1,1,L)
 
 % received signal
-r = g.*data + n;
+r = g.*tx_data + n;
