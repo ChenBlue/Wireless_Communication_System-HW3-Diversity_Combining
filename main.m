b4381aa28b06037475a52681cc5c1454fe141e46
ChenBlue <bob840806@gmail.com>
2017-12-27 01:09:42 +0800

    modify variance & mean and plot more graph

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-sample_num = 5000000;
+sample_num = 1000000;
 
 % Preallocation for error of probability
 Pe_sc = zeros(5, 4, 2);
@@ -8,6 +8,11 @@ Pe_dc = zeros(5,4, 2);
 tic
 
 for R = 0:1
+    if R == 0
+        sigma = 1/sqrt(2);
+    else
+        sigma = 1/2;
+    end
     for enr_index = 1:5
         enr_dB = (enr_index-1)*2+1; % energy-to-noise ratio:1, 3, 5, 7, 9 dB
         enr = 10^(enr_dB/10); 
@@ -24,7 +29,7 @@ for R = 0:1
             n = normrnd(0,sqrt(En/2),2,sample_num,L) + 1i*normrnd(0,sqrt(En/2),2,sample_num,L);
 
             % generate fading gain (R=0: Rayleigh; R=1: Riciean)
-            g = normrnd(R,1,1,sample_num,L) + 1i*normrnd(R,1,1,sample_num,L);
+            g = normrnd(R/2,sigma,1,sample_num,L) + 1i*normrnd(R/sqrt(2),1/2,1,sample_num,L);
             g_tmp = repmat(g,2,1,1); % replicate for 2 bits
 
             tx_data = repmat(data,1,1,L); % replicate for L branches
@@ -69,58 +74,84 @@ end
 toc
 
 % Plot Rayleigh
-figure,plot(Pe_sc(:,:,1),'-*')
+figure,plot([1 3 5 7 9], Pe_sc(:,:,1),'-*')
 set(gca, 'YScale', 'log')
 title('BER of Selective Combining (Rayleigh)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_mrc(:,:,1),'-*')
+figure,plot([1 3 5 7 9], Pe_mrc(:,:,1),'-*')
 set(gca, 'YScale', 'log')
 title('BER of Maximal Ratio Combining (Rayleigh)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_egc(:,:,1),'-*')
+figure,plot([1 3 5 7 9], Pe_egc(:,:,1),'-*')
 set(gca, 'YScale', 'log')
 title('BER of Equal Gain Combining (Rayleigh)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_dc(:,:,1),'-*')
+figure,plot([1 3 5 7 9], Pe_dc(:,:,1),'-*')
+set(gca, 'YScale', 'log')
 title('BER of Direct Combining (Rayleigh)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
 % Plot Riciean
-figure,plot(Pe_sc(:,:,2),'-*')
+figure,plot([1 3 5 7 9], Pe_sc(:,:,2),'-*')
 set(gca, 'YScale', 'log')
-title('BER of Selective Combining (Rayleigh)');
+title('BER of Selective Combining (Ricean)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_mrc(:,:,2),'-*')
+figure,plot([1 3 5 7 9], Pe_mrc(:,:,2),'-*')
 set(gca, 'YScale', 'log')
-title('BER of Maximal Ratio Combining (Rayleigh)');
+title('BER of Maximal Ratio Combining (Ricean)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_egc(:,:,2),'-*')
+figure,plot([1 3 5 7 9], Pe_egc(:,:,2),'-*')
 set(gca, 'YScale', 'log')
-title('BER of Equal Gain Combining (Rayleigh)');
+title('BER of Equal Gain Combining (Ricean)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
-figure,plot(Pe_dc(:,:,2),'-*')
-title('BER of Direct Combining (Rayleigh)');
+figure,plot([1 3 5 7 9], Pe_dc(:,:,2),'-*')
+set(gca, 'YScale', 'log')
+title('BER of Direct Combining (Ricean)');
 legend('L=1','L=2','L=3','L=4');
 xlabel('SNR (dB)');
 ylabel('Bit error rate');
 
+% Comparison among all combining strategies
+% Rayleigh
+figure,plot([1 3 5 7 9], Pe_dc(:,4,1),'-*',[1 3 5 7 9], Pe_sc(:,4,1),'-+',[1 3 5 7 9], Pe_egc(:,4,1),'-*',[1 3 5 7 9], Pe_mrc(:,4,1),'-+')
+set(gca, 'YScale', 'log')
+title('BER of SC & MRC & EGC & DC (Rayleigh)');
+legend('Direct Combining','Selective Combining','Equal Gain Combinig','Maximal Ratio Combining');
+xlabel('SNR (dB)');
+ylabel('Bit error rate');
+
+% Ricean
+figure,plot([1 3 5 7 9], Pe_dc(:,4,2),'-*',[1 3 5 7 9], Pe_sc(:,4,2),'-+',[1 3 5 7 9], Pe_egc(:,4,2),'-*',[1 3 5 7 9], Pe_mrc(:,4,2),'-+')
+set(gca, 'YScale', 'log')
+title('BER of SC & MRC & EGC & DC (Ricean)');
+legend('Direct Combining','Selective Combining','Equal Gain Combinig','Maximal Ratio Combining');
+xlabel('SNR (dB)');
+ylabel('Bit error rate');
+
+% Comparison between Rayleigh and Ricean
+figure,plot([1 3 5 7 9], Pe_egc(:,4,1),'-*',[1 3 5 7 9], Pe_egc(:,4,2),'-+')
+set(gca, 'YScale', 'log')
+title('BER of Rayleigh and Ricean');
+legend('Rayleigh','Ricean');
+xlabel('SNR (dB)');
+ylabel('Bit error rate');
