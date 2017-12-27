647c3b007855f637d8c237d152b853ea48850cd5
ChenBlue <bob840806@gmail.com>
2017-12-27 20:58:52 +0800

    rename get_error_prob.m to get_BER.m

--- get_BER.m
+++ get_BER.m
@@ -1,4 +1,4 @@
-function Pe = get_error_prob(A,B,sample_num)
+function Pe = get_BER(A,B,sample_num)
 % Calculate bit error rate by counting how many elements in two matrices A
 % and B are different. Next, divided the size the matrix to derive BER.
 wrong = A~= B;
